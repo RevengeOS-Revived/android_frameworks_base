vijaymalav564/android_frameworks_base-2

Disable PixelProps when PLATFORM_SECURITY_PATCH_OVERRIDE is set (#714)

core/java/android/app/Instrumentation.java
@@ -1159,8 +1159,11 @@ public class Instrumentation {
         Application app = getFactory(context.getPackageName())
                 .instantiateApplication(cl, className);
         app.attach(context);
-        String packageName = app.getPackageName();
-        PixelPropsUtils.setProps(packageName);
+        String patchCrDroid = Build.VERSION.SECURITY_PATCH_CRDROID;
+        if ("".equals(patchCrDroid)) {
+            String packageName = app.getPackageName();
+            PixelPropsUtils.setProps(packageName);
+        }
         return app;
     }
     
@@ -1178,8 +1181,11 @@ public class Instrumentation {
             ClassNotFoundException {
         Application app = (Application)clazz.newInstance();
         app.attach(context);
-        String packageName = app.getPackageName();
-        PixelPropsUtils.setProps(packageName);
+        String patchCrDroid = Build.VERSION.SECURITY_PATCH_CRDROID;
+        if ("".equals(patchCrDroid)) {
+            String packageName = app.getPackageName();
+            PixelPropsUtils.setProps(packageName);
+        }
         return app;
     }
 

RevengeOS-Revived/android_frameworks_base

core/java/android/app/Instrumentation.java
@@ -1162,8 +1162,11 @@ public class Instrumentation {
                 .instantiateApplication(cl, className);
         app.attach(context);
         maybeSpoofBuild(app);
-        String packageName = app.getPackageName();
-        PixelPropsUtils.setProps(packageName);
+        String patchCustom = Build.VERSION.SECURITY_PATCH_CUSTOM;
+        if ("".equals(patchCustom)) {
+            String packageName = app.getPackageName();
+            PixelPropsUtils.setProps(packageName);
+        }
         return app;
     }
     
@@ -1182,8 +1185,11 @@ public class Instrumentation {
         Application app = (Application)clazz.newInstance();
         app.attach(context);
         maybeSpoofBuild(app);
-        String packageName = app.getPackageName();
-        PixelPropsUtils.setProps(packageName);
+        String patchCustom = Build.VERSION.SECURITY_PATCH_CUSTOM;
+        if ("".equals(patchCustom)) {
+            String packageName = app.getPackageName();
+            PixelPropsUtils.setProps(packageName);
+        }
         return app;
     }
 